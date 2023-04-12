128519bfaab21d6588b39c20ee4b204ef443ab8c
Emir Yasin Bolat <89213834+speeedev@users.noreply.github.com>
2023-04-13 00:48:04 +0300

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,43 @@
+﻿$(function () {
+  $(".btn-close").click(function () {
+    var btn = $(this);
+    var id = btn.data("id");
+    var action = btn.data("action");
+    var url = "/" + action + "/" + id;
+
+    // alert(url);
+    $.ajax({
+      type: "get",
+      url: url,
+
+      success: function (data) {
+        // alert(data);
+        btn.parent().remove();
+      },
+    });
+  });
+  $("li").click(function () {
+    var btn = $(this);
+    var id = btn.data("id");
+    var action = btn.data("action");
+    var url = "/" + action + "/" + id;
+
+    // alert(url);
+    $.ajax({
+      type: "get",
+      url: url,
+
+      success: function (data) {
+        // alert(data);
+        if (data == "False") {
+          btn.attr("class", "list-group-item list-group-item-success m-1");
+        } else {
+          btn.attr(
+            "class",
+            "list-group-item list-group-item-danger text-decoration-line-through m-1"
+          );
+        }
+      },
+    });
+  });
+});
